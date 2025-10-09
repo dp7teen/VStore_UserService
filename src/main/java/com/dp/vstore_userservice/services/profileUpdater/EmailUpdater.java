369de0bed368fa0fb2821450489335598a8ca4bd
Deviprasath <deviprasath@screen-magic.com>
2025-10-09 09:47:24 +0530

    Bug: emailUpdater does not check if the edited email is already present in the database with another user.  it is resolved now.

--- src/main/java/com/dp/vstore_userservice/services/profileUpdater/EmailUpdater.java
+++ src/main/java/com/dp/vstore_userservice/services/profileUpdater/EmailUpdater.java
@@ -2,12 +2,23 @@ package com.dp.vstore_userservice.services.profileUpdater;
 
 import com.dp.vstore_userservice.dtos.UpdateProfileDto;
 import com.dp.vstore_userservice.models.User;
+import com.dp.vstore_userservice.repositories.UserRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailUpdater implements ProfileUpdater{
+    private final UserRepository userRepository;
+
+    public EmailUpdater(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void update(User user, UpdateProfileDto dto) {
+        if (dto.getEmail() != null && userRepository.existsByEmail(dto.getEmail())) {
+            throw new IllegalArgumentException("Bad Credentials!");
+        }
+
         if (dto.getEmail() != null) {
             user.setEmail(dto.getEmail());
         }
